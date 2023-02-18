323f75853d965aa9094fdc15634bcf516e7237e7
Eduardo <eduardo.vazquez.7e5@itb.cat>
2023-02-18 11:51:25 +0100

    Finished Game

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -40,7 +40,7 @@ public class GameManager : Singleton<GameManager>
 
     IEnumerator LoadNewScene()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(15.0f);
         SceneManager.LoadScene(0);
     }
 }
